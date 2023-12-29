00dca682bebe1a419f58312d3c3a7d79a2031bfd
JorgeProdri <piguave970@gmail.com>
2023-12-29 13:38:33 -0500

    create

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,42 @@
 import Inicio from './pages/home/Inicio';
 import Finca from './pages/finca/Finca';
 import Lotes from './pages/lotes/usuario/Lotes';
+import Usuario from './pages/usuario/Usuario';
+import Login from './pages/login/Login';
+//navb and menu
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Menu from './components/menu/Menu';
+
+//stilos
+import "./styles/global.scss"
+
+//rutas
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet
 } from "react-router-dom";
+
 
 function App() {
 
   const Layout = ()=> {
     return (
       <div className="main">
-      
+        <Navbar/>
+        <div className="container">
+          
+          <div className="menucontainer">
+            <Menu/>
+          </div>
+          <div className="contencontainer">
+            <Outlet/>
+
+          </div>
+          
+        </div>
+        <Footer/>   
       </div>
     )   
   }
@@ -24,23 +47,35 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <Inicio/> 
-      ),
+      element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <Inicio/>
+        },
+        {
+          path: "/finca",
+          element: <Finca/>
+        },
+        {
+          path: "/lotes",
+          element: <Lotes/>
+        },
+        {
+          path: "/usuario",
+          element: <Usuario/>
+        },
+      ]
     },
     {
-      path: "finca",
-      element: <Finca/> 
+      path: "/login",
+      element: <Login/>
     },
-    {
-      path: "lotes",
-      element: <Lotes/> 
-    },
+   
   ]);
 
-  return (
-   <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router}/>
+  
 }
 
 export default App
